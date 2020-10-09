0fad870bf699584980190eea5887b97839683047
Olavo Brito Souza <olavo.souza@opus-software.com.br>
2020-10-09 09:52:23 -0300

    Remove wrong folder

--- spring-data-overview/src/main/java/com/pluralsight/springdataoverview/repository/FlightRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight.springdataoverview.repository;
-
-import com.pluralsight.springdataoverview.entity.Flight;
-import org.springframework.data.repository.CrudRepository;
-
-public interface FlightRepository extends CrudRepository<Flight, Long> {
-}
